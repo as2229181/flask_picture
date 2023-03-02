48a21a78ff4628ee340254b0fa94cff909db55dd
as2229181 <as2229181@gmail.com>
2023-03-02 22:53:00 +0800

    increase user create

--- apps/views/user.py
+++ apps/views/user.py
@@ -1,5 +1,12 @@
-from flask import render_template
+from flask import render_template,request
 
 class views:
     def index():
         return render_template("user/index.html")
+    def new():
+        return render_template("user/new.html")
+    def create(self):
+        username = request.form["username"]
+        email=request.form["email"]
+        password=request.formp["password"]
+        return "create.{}".format(username)
--- router.py
+++ router.py
@@ -1,10 +1,16 @@
 from apps import app
 from apps.views.user import views
 
-@app.route("/",methods=["get"])
+@app.route("/",methods=["GET"])
 def index():
     return views.index()
 
+@app.route("/new",methods=["GET"])
+def new():
+    return views.new()
+@app.route("/create",methods=["POST"] )
+def create():
+    return views.create()
 
 
 app.run(debug=True)
